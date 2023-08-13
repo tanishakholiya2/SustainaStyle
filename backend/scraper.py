86674c45ea3e5c7fcf3bce7120041adacb9a83a7
Paarth Mishra <pmshot607@gmail.com>
2023-08-12 22:50:01 -0700

    scrape

--- backend/scraper.py
+++ backend/scraper.py
@@ -13,28 +13,32 @@ def get_database():
    # Create the database for our example (we will use the same database throughout the tutorial
    return client['SustainaStyle']
 
-headers = {
-    "User-Agent":
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
-}
-db = get_database()
-stores = db["stores"]
-baseURL = 'https://google.com/search?q='
 
-for store in stores.find():
-    time.sleep(1)
-    query = "site:"+store["link"]+" white tshirt"
-    query=query.replace(":", "%3A")
-    query = query.replace(" ", "+")
-    url = baseURL+query
-    res = requests.get(url, headers=headers)
-    soup = BeautifulSoup(res.text, "html.parser")
-    div = soup.find('div', class_="MjjYud")
-    div2 = div.find('div', class_="yuRUbf")
-    a = div2.find('a')
-    img = div.find('img')
-    src = img['src']
-    print([a, src])
+def fetch_results(inc):
+    headers = {
+        "User-Agent":
+        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
+    }
+    db = get_database()
+    stores = db["stores"]
+    baseURL = 'https://google.com/search?q='
+    result = []
+    for store in stores.find():
+        query = inc
+        print(store)
+        time.sleep(1)
+        query = "site:"+store["link"]+" "+query
+        query=query.replace(":", "%3A")
+        query = query.replace(" ", "+")
+        url = baseURL+query
+        res = requests.get(url, headers=headers)
+        soup = BeautifulSoup(res.text, "html.parser")
+        main = soup.find('div', {"id": "search"})
+        div1 = main.find_all("div", class_="MjjYud")
+        div1 = div1[1]
+        div2 = div1.find('div', class_="yuRUbf")
+        a = div2.find('a')["href"]
+        a2 = div1.find_all('a', {'href': a})
+        result.append({"link": a})
 
-
-
+    return result
